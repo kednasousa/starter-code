dd5707b1cb0ff127c5ff7eb3523820f929064f14
Kedna Cleia <kednacsousa@hotmail.com>
2024-11-17 15:43:39 -0300

    adiciona script para navegação dinâmica na página da tripulação

---
+++ assets/js/crew.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll('.nav-link');
+  const destinationSections = document.querySelectorAll('.container_crew > section');
+
+  links.forEach(link => {
+    link.addEventListener('click', function (event) {
+      const targetId = this.id ? this.id.replace('-link', '') : null;
+      const targetSection = targetId ? document.getElementById(targetId) : null;
+
+      if (targetSection && destinationSections.length > 0) {
+        // Se a seção alvo existir e estivermos na página de destino, previna o comportamento padrão e alterne as seções
+        event.preventDefault();
+
+        // Remove a classe 'active' de todos os links
+        links.forEach(link => link.classList.remove('active'));
+
+        // Adiciona a classe 'active' ao link clicado
+        this.classList.add('active');
+
+        // Oculta todas as seções
+        destinationSections.forEach(section => section.style.display = 'none');
+
+        // Mostra a seção correspondente ao link clicado
+        targetSection.style.display = 'block';
+      } else {
+        // Se a seção alvo não existir, permita a navegação padrão
+        // Isso significa que o link levará a uma página diferente
+      }
+    });
+  });
+});
